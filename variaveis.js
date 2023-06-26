c60065316c02adbe6a993c4bf154616959d25742
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-06-26 12:04:04 -0300

    second-commit

--- variaveis.js
+++ variaveis.js
@@ -1,20 +1,20 @@
-let age =20; // number
-let fullName = 'Lindomar'; // string
-let isValid = true; // boolean
-let xablou; // undefined
-let value = null;
+//let age =20; // number
+// fullName = 'Lindomar'; // string
+//let isValid = true; // boolean
+//let xablou; // undefined
+//let value = null;
 
 //console.log(value);
 
  
-var userEmail = 'maria@gmail.com';
-let userId = 78945;
-const userLocation = 'Brasil'
+//var userEmail = 'maria@gmail.com';
+//let userId = 78945;
+//const userLocation = 'Brasil'
 
-if (true){
-    var userAge = '20';
+//if (true){
+  //  var userAge = '20';
     //console.log(userAge)
-}
+//}
 
 //console.log(userAge);
 
@@ -31,21 +31,53 @@ if (true){
 
 
 
-var name = 'João';
+//var name = 'João';
 
-if (true) {
-  var name = 'Maria';
-  console.log(name); // Output: Maria
-}
+//if (true) {
+  //var name = 'Maria';
+  //console.log(name); // Output: Maria
+//}
 
-console.log(name); // Output: Maria
+//console.log(name); // Output: Maria
 
 // Exemplo com let
-let name2 = 'João';
+//let name2 = 'João';
 
-if (true) {
-  let name2 = 'Maria';
-  console.log(name2); // Output: Maria
-}
+//if (true) {
+  //let name2 = 'Maria';
+  //console.log(name2); // Output: Maria
+//}
 
-console.log(name2); // Output: João
+//console.log(name2); // Output: João
+
+
+
+
+//const favoriteLanguage ='javascript';
+//favoriteLanguage = 'python';
+
+//console.log(favoriteLanguage)
+
+//let favoriteTecnology ='Machine learning';
+//favoriteTecnology = 'facial recognition';
+
+//console.log(favoriteTecnology)
+
+//let patientId =50;
+//let isEnrolled = true;
+//let patientEmail = 'ana@gmail.com'
+
+
+//console.log(typeof patientId);
+
+
+const myName = 'Fellipe';
+const birthCity = 'Goiana-PE'
+const birthYear = 1989; 
+
+console.log(typeof myName);
+console.log(birthCity);
+console.log(typeof birthYear);
+
+
+
