302b1cdc7c3e1bcad912827b632a7c03c420b00a
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-06-26 11:23:25 -0300

    first-commit

---
+++ variaveis.js
@@ -0,0 +1,51 @@
+let age =20; // number
+let fullName = 'Lindomar'; // string
+let isValid = true; // boolean
+let xablou; // undefined
+let value = null;
+
+//console.log(value);
+
+ 
+var userEmail = 'maria@gmail.com';
+let userId = 78945;
+const userLocation = 'Brasil'
+
+if (true){
+    var userAge = '20';
+    //console.log(userAge)
+}
+
+//console.log(userAge);
+
+//var userName = 'Isabella';
+//console.log(userName);
+
+//var userName = 'Lucas';
+//console.log(userName);
+
+// Exemplo com var
+
+
+
+
+
+
+var name = 'João';
+
+if (true) {
+  var name = 'Maria';
+  console.log(name); // Output: Maria
+}
+
+console.log(name); // Output: Maria
+
+// Exemplo com let
+let name2 = 'João';
+
+if (true) {
+  let name2 = 'Maria';
+  console.log(name2); // Output: Maria
+}
+
+console.log(name2); // Output: João
