b4da4428ec653a723c67e083672b15664ec58a80
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-06-27 12:09:16 -0300

    ifelse

--- operaMatematica.js
+++ operaMatematica.js
@@ -1,86 +1,160 @@
-// const base = 5;
-// const height = 8;
-// const area = base * height; 
-// const perimeter = 5 + 5 + 8 + 8;
+// // const base = 5;
+// // const height = 8;
+// // const area = base * height; 
+// // const perimeter = 5 + 5 + 8 + 8;
 
-// console.log(area);
-// console.log(perimeter);
+// // console.log(area);
+// // console.log(perimeter);
 
-// const age = 61;
+// // const age = 61;
 
-// if (age >= 18 && age < 25){
-//     console.log('maior de idade')
-// } else if (age >= 25 && age <=60){
-//     console.log('Adulto')
-// }else if (age > 60){
-//     console.log('Idoso')
-// }
+// // if (age >= 18 && age < 25){
+// //     console.log('maior de idade')
+// // } else if (age >= 25 && age <=60){
+// //     console.log('Adulto')
+// // }else if (age > 60){
+// //     console.log('Idoso')
+// // }
 
 
-// const numeroInteiro =65;
+// // const numeroInteiro =65;
 
-// if (numeroInteiro % 2 === 0){
-//     console.log('seu número é inteiro');
+// // if (numeroInteiro % 2 === 0){
+// //     console.log('seu número é inteiro');
 
-// }else(console.log('seu número é impar'));
+// // }else(console.log('seu número é impar'));
 
-// const beenPrice =10; 
-// const totalMoney = 10;
+// // const beenPrice =10; 
+// // const totalMoney = 10;
+// // let message ='';
+
+
+// // if(totalMoney < beenPrice){
+// //     message ='Você não possui saldo suficiente';
+// // }else if (totalMoney === beenPrice){
+// //     message ='Você vai gastar todo o seu dinheirinho';
+// // }else {
+// //     message = 'Pode comprar'
+// // };
+
+// // console.log(message);
+
+
+// // const pastLifes = 1;
+// // const resultLifes = 2;
+// // const allLifes = 450
+// // let acenption = '';
+
+// // if(pastLifes + resultLifes >= allLifes){
+// //     acenption = 'Você está assencionado'
+
+// // }else if (pastLifes + resultLifes <= 10){
+// //     acenption = 'Irá para o umbral'
+// // }
+
+// // console.log(acenption);
+
+
+
+// // const carteira = 120;
+// // const contaBanco = 1200;
+// // let gastos = 5000;
+// // let contaRestaurante = '';
+
+// // if (carteira >= gastos)
+// // {
+// //     contaRestaurante = 'Mandou bem pra gata'
+
+// // }else if(carteira + gastos <=contaBanco){
+// //     contaRestaurante = 'passe o cartão'
+
+// // }else {
+// //     contaRestaurante = 'Vai ter q lavar prato'
+// // }
+
+// // console.log(contaRestaurante)
+
+// // let temperature = 20;
+// // let weather = temperature > 30 ? 'ta quente' : 'ta frio';
+
+// // console.log(weather);
+
+// let idadeCorno = 52;
+// let serCorno = idadeCorno >= 51 ? 'É COORNO!' : 'Ainda não é corno!';
+
+// console.log(`A idade de ser corno é ${idadeCorno}, você ${serCorno}`)
+
+// let idadeVote = 15;
+// let canVote = idadeVote>= 18 ? 'Já pode votar' : 'Ainda não pode votar';
+
+// console.log(` Esse viado tem ${idadeVote}, Esse viado ${canVote}`);
+
+
+// let fenderGuitar = false;
+// let temFender = fenderGuitar === true? 'Você tem uma fender, parabéns' : 'você não tem uma fender, se contente com guitara chinesa';
+
+// console.log(`Uau!! é ${fenderGuitar}, ${temFender} `);
+
+
+// let temperature = 20;
+// let weather = temperature >=35 && 'ta quente';
+
+// console.log(weather);
+
+
+// let grade =60;
 // let message ='';
 
+// if(grade >=80){
 
-// if(totalMoney < beenPrice){
-//     message ='Você não possui saldo suficiente';
-// }else if (totalMoney === beenPrice){
-//     message ='Você vai gastar todo o seu dinheirinho';
-// }else {
-//     message = 'Pode comprar'
-// };
+//     message= 'Aprovado'
+
+// }else if (grade < 80 && grade >= 60){
+    
+//     message = 'Você está na nossa lista de espera'
+
+// }else if(grade < 60){
+//     message = 'Você está reprovado'
+    
+// }
 
 // console.log(message);
 
 
-// const pastLifes = 1;
-// const resultLifes = 2;
-// const allLifes = 450
-// let acenption = '';
 
-// if(pastLifes + resultLifes >= allLifes){
-//     acenption = 'Você está assencionado'
+// const food = 'pão';
+// const drink ='café';
 
-// }else if (pastLifes + resultLifes <= 10){
-//     acenption = 'Irá para o umbral'
+// if(drink === 'café' && food === 'pão'){
+//     console.log('obrigado');
+// }else{
+//     console.log('Erraram meu pedido!');
 // }
 
-// console.log(acenption);
+// const carrot = true;
+// const rice = false;
+// const been = true;
+
+// let elements = carrot && rice && been;
+
+// console.log(elements);
 
 
+// let currentHour = 14;
+// let message ='';
 
-// const carteira = 120;
-// const contaBanco = 1200;
-// let gastos = 5000;
-// let contaRestaurante = '';
-
-// if (carteira >= gastos)
-// {
-//     contaRestaurante = 'Mandou bem pra gata'
-
-// }else if(carteira + gastos <=contaBanco){
-//     contaRestaurante = 'passe o cartão'
-
-// }else {
-//     contaRestaurante = 'Vai ter q lavar prato'
+// if(currentHour >=22){
+//     message='Não deveriamos comer nada, é hora de dormir';
+// }else if(currentHour >=18 && currentHour < 22){
+//     message='Rango da noite, vamos jantar';
+// }else if (currentHour >=14 && currentHour < 18){
+//     message='Vamos fazer um bolo para o café da tarde?';
+// }else if (currentHour>= 11 && currentHour <14){
+//     message='Hora do almoço';
+// }else if (currentHour >=4 && currentHour <=11){
+//     message ='Cheiro de café recém-passado';
+// }else{
+//     message='Dormindo um sono profundo'
 // }
 
-// console.log(contaRestaurante)
-
-// let temperature = 20;
-// let weather = temperature > 30 ? 'ta quente' : 'ta frio';
-
-// console.log(weather);
-
-let idadeCorno = 25;
-let serCorno = idadeCorno >= 51 ? 'É COORNO!' : 'Ainda não é corno!';
-
-console.log(`A idade de ser corno é ${idadeCorno}, você ${serCorno}`)
-
+// console.log(message);
