fb7ce1e88145dd489ca3d98d8bf94d1a3e4b1730
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-06-26 21:28:39 -0300

    serguraAi

--- operaMatematica.js
+++ operaMatematica.js
@@ -1,2 +1,75 @@
-const soma = 50; 
-const subtracao = 80;
+// const base = 5;
+// const height = 8;
+// const area = base * height; 
+// const perimeter = 5 + 5 + 8 + 8;
+
+// console.log(area);
+// console.log(perimeter);
+
+// const age = 61;
+
+// if (age >= 18 && age < 25){
+//     console.log('maior de idade')
+// } else if (age >= 25 && age <=60){
+//     console.log('Adulto')
+// }else if (age > 60){
+//     console.log('Idoso')
+// }
+
+
+// const numeroInteiro =65;
+
+// if (numeroInteiro % 2 === 0){
+//     console.log('seu número é inteiro');
+
+// }else(console.log('seu número é impar'));
+
+// const beenPrice =10; 
+// const totalMoney = 10;
+// let message ='';
+
+
+// if(totalMoney < beenPrice){
+//     message ='Você não possui saldo suficiente';
+// }else if (totalMoney === beenPrice){
+//     message ='Você vai gastar todo o seu dinheirinho';
+// }else {
+//     message = 'Pode comprar'
+// };
+
+// console.log(message);
+
+
+// const pastLifes = 1;
+// const resultLifes = 2;
+// const allLifes = 450
+// let acenption = '';
+
+// if(pastLifes + resultLifes >= allLifes){
+//     acenption = 'Você está assencionado'
+
+// }else if (pastLifes + resultLifes <= 10){
+//     acenption = 'Irá para o umbral'
+// }
+
+// console.log(acenption);
+
+
+
+const carteira = 120;
+const contaBanco = 1200;
+let gastos = 5000;
+let contaRestaurante = '';
+
+if (carteira >= gastos)
+{
+    contaRestaurante = 'Mandou bem pra gata'
+
+}else if(carteira + gastos <=contaBanco){
+    contaRestaurante = 'passe o cartão'
+
+}else {
+    contaRestaurante = 'Vai ter q lavar prato'
+}
+
+console.log(contaRestaurante)
