10dd1e1851139e7616e5e560f3902a830eaa725f
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-07-04 20:43:41 -0300

    loopFor

---
+++ arrays.js
@@ -0,0 +1,42 @@
+// const product = 'iPhone 32';
+// const price = 58000;
+// const discount = 10;
+
+// const message =
+
+// `Produto: ${product}
+// preço: ${price}
+// Desconto: ${discount}%
+// Preço com desconto: R$ ${((price -(price *(discount/100)))).toFixed(2)}`;
+
+// console.log(message);
+
+// const shoppingList = ['Arroz', 'batata' , 'Leite Ninho' , 'Sucrilhos'];
+
+
+// shoppingList.push('Suco de laranja'); // adiciona um array no final
+// shoppingList.unshift() // adiciona um elemento no inicio do array
+// shoppingList.pop() //revome o último elemento do array
+// shoppingList.shift() // remove o primeiro elemento do array
+
+// console.log(shoppingList[shoppingList.length -1 ]);
+
+
+//const menu = ['home', 'Serviços' , 'Porifólio', 'Links'];
+////const menuServiços =menu[0];
+//console.log(menuServiços);
+
+// const menu = ['home', 'Serviços' , 'Porifólio', 'Links'];
+
+// menu.push('Contato');
+
+// console.log(menu);
+
+
+const laps = 60;
+
+for( let index =1; index <= laps; index +=1){
+console.log(`Volta ${index} completa!`);
+}
+
+console.log('Acabou a corrida!');
