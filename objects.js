ff39299af82e70eff95532e41d54365b4e0c7e57
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-07-20 21:03:07 -0300

    secondObjetcsCommmit

---
+++ objects.js
@@ -0,0 +1,69 @@
+// const account ={
+//     agency: '0985',
+//     bank:{
+//         cod: '012',
+//         id:4,
+//         name: 'TyrbeBank'
+//     },
+// };
+
+// console.log(account.bank.cod);
+
+// const user = {
+//     id: 99,
+//     email: 'saintlucas@hotmail.com',
+//     info:{
+//         name: 'Fellipe',
+//         lasName: 'Barnabé',
+//         address:{
+//             street: 'Rua Luiz Gomes, 116 - térreo',
+//             district: 'Goiana',
+//             city: 'Centro',
+//             state: 'Pernambuco',
+        
+//         },
+//     },
+// };
+
+
+// console.log(user.info.address.city);
+
+
+const residents = [
+    {
+        name:'Fellipe',
+        lasName: 'Barnabé',
+        floor: 10,
+        apartment: 1005,
+
+    },
+    {
+        name: 'Sandra',
+        lasName:'Barnabé',
+        floor: 5,
+        apartment: 502,
+    },
+    {
+        name:'Willian',
+        Lastname: 'Albuquerque',
+        floor: 5,
+        apartment: 502,
+    },
+    {
+        name: 'Murilo',
+        lasName: 'Ferraz',
+        floor: 8,
+        apartment: 804,
+    },
+    {
+        name: 'Zoey',
+        LastName: 'Brooks',
+        floor:1,
+        apartment: 101,
+    },
+];
+// const firstResident = residents[0];
+// console.log(firstResident);
+
+const secondResident = residents [0];
+console.log(secondResident);
